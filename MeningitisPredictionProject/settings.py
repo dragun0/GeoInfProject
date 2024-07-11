9b7224dc99d99177bc29c69372637962118ddb1b
Leonie Dragun <82779352+dragun0@users.noreply.github.com>
2024-07-12 00:05:32 +0200

    Config for railway deployment

--- MeningitisPredictionProject/settings.py
+++ MeningitisPredictionProject/settings.py
@@ -14,17 +14,20 @@ from pathlib import Path
 import os
 from celery.schedules import crontab
 from celery import Celery
+from dotenv import load_dotenv
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h7_741@4j*@lyn$%f#me&l2rv2djf_uuf48=5)0(dz!211($#4'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -56,7 +59,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-   # 'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'MeningitisPredictionProject.urls'
@@ -86,14 +88,15 @@ WSGI_APPLICATION = 'MeningitisPredictionProject.wsgi.application'
 
 DATABASES = {
    'default': {
-       'ENGINE': 'django.contrib.gis.db.backends.postgis',
-       'NAME': 'postgres',
+       'ENGINE': os.environ.get("ENGINE"),
+       'NAME': os.environ.get("NAME"),
        'USER': 'geoinf_postgre',
-       'PASSWORD': 'Useradmin1g',
-       'HOST': 'geoinfdb.postgres.database.azure.com',
-       'PORT': '5432',
+       'PASSWORD': os.environ.get("PASSWORD"),
+       'HOST': os.environ.get("HOST"),
+       'PORT': os.environ.get("PORT"),
    }
 }
+
 
 
 #DATABASES = {
