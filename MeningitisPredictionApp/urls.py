ab7d5380247d70ae32dff7fa223a12d85f1a5d44
Leonie Dragun <82779352+dragun0@users.noreply.github.com>
2024-07-12 13:22:22 +0200

    changed app's url patterns

--- MeningitisPredictionApp/urls.py
+++ MeningitisPredictionApp/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('MeningitisPredictionApp/', views.mapView, name='RiskMap'),
-    path('MeningitisPredictionApp/Article/<int:article_id>/', views.articleView, name='article'),
-    path('MeningitisPredictionApp/Methodology/<int:metho_id>/', views.methodologyView, name='methodology'),
-    path('MeningitisPredictionApp/Weather', views.weatherView, name='weather'),
+    path('', views.mapView, name='RiskMap'),
+    path('Article/<int:article_id>/', views.articleView, name='article'),
+    path('Methodology/<int:metho_id>/', views.methodologyView, name='methodology'),
+    path('Weather', views.weatherView, name='weather'),
 ]
