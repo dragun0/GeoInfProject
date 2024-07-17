a05a9a0d88b62a695f7f63c00d62f63a9f5415bd
Leonie Dragun <82779352+dragun0@users.noreply.github.com>
2024-07-17 13:26:23 +0200

    corrected accidental delete

--- MeningitisPredictionProject/settings.py
+++ MeningitisPredictionProject/settings.py
@@ -156,6 +156,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # for django-raster package 
 RASTER_USE_CELERY = True
 
+CELERY_BROKER_URL = os.environ['REDIS_URL'] #'redis://localhost:6379/0' 
+CELERY_RESULT_BACKEND = os.environ['REDIS_URL'] #'redis://localhost:6379/0' 
 #CELERY_ACCEPT_CONTENT = ['json']
 #CELERY_TASK_SERIALIZER = 'json'
 #CELERY_RESULT_SERIALIZER = 'json'
